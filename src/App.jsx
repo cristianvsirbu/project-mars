5c3b44edd6eef2d9d11df2305f22e1f022e2ca00
Cristian Sîrbu <cristiansirbu4@gmail.com>
2023-08-10 15:51:53 +0300

    Added comments and removed unused assets

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
   return (
     <div>
       <Navbar />
+      {/* Line */}
       <img className="hidden md:block 2xl:flex mx-auto my-0 py-0" src="src/assets/line.png" />
       <div>
         <Outlet />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,7 @@ import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import menu from "../assets/menu.webp";
 import close from "../assets/close.webp";
-import NASA from "../assets/NASA.svg";
-import x from "../assets/x.svg";
+
 
 
 
@@ -33,17 +32,56 @@ const Navbar = () => {
   return (
 
     <>
-      <nav className="flex h-24 justify-between mx-6 md:mx-10 xl:h-32 2xl:h-36">
-          <img src="/src/assets/logo.png" className="h-20 md:h-24 mt-2 xl:h-32 xl:ml-12 xl:mt-4 2xl:h-40 2xl:ml-32"/>
-        <ul className="hidden md:flex items-center w-1/2 justify-between 2xl:justify-around">
+      
+      {/* Navbar */}
+      <nav
+        className="flex
+                   h-24
+                   justify-between
+                   mx-6
+                   md:mx-10
+                   xl:h-32
+                   xl:mx-2xl:h-36">
+        
+        {/* Logo */}
+        <img src="/src/assets/logo.png"
+          className="h-20 
+                     md:h-24
+                     mt-2
+                     xl:h-32
+                     xl:ml-12
+                     xl:mt-4
+                     2xl:h-40
+                     2xl:ml-32"/>
+        
+        {/* Menu */}
+        <ul className="hidden 
+                       md:flex
+                       items-center
+                       w-1/2
+                       justify-between
+                       2xl:justify-around">
           {navLinks.map(link => (
-            <li className={` text-white text-decoration-none font-regular text-[16px] lg:text-[18px] xl:text-[24px] 2xl:text-[34px]`} key={link.to}>
+            <li className={` text-white 
+                               text-decoration-none
+                               font-regular
+                               text-[16px]
+                               lg:text-[18px]
+                               xl:text-[24px]
+                               2xl:text-[34px]`}
+              key={link.to}
+            >
               <NavLink to={link.to}>{link.text}</NavLink>
             </li>
           ))}
         </ul>
+
         {/* Hamburger Menu */}
-        <div className="md:hidden flex flex-1 justify-end items-center">
+        <div className="md:hidden
+                        flex
+                        flex-1
+                        justify-end
+                        items-center">
           <button className='h-12 w-12' onClick={toggleMenu}>{menuOpen ? (<img src={close} />) : (<img src={menu} />)}</button>
         </div>
       </nav>
