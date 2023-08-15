c49929dda06c820b55148d39f99a7464784e648b
Cristian Sîrbu <cristiansirbu4@gmail.com>
2023-08-15 13:32:05 +0300

    Added large layouts

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,24 @@
-
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div className="flex flex-col-reverse">
-      <div className="p-4 flex flex-col">
+    <div className="flex flex-col-reverse xl:flex-row xl:justify-center">
+      <div className="p-4 flex flex-col w-full xl:w-1/2 4k:justify-center">
         {/* Title */}
         <div className="flex flex-col items-center py-12">
           <h2 className="text-white font-bold text-3xl">PROJECT</h2>
           <img src="src/assets/line2.svg" />
           <h1 className="text-white font-extrabold text-6xl">MARS</h1>
         </div>
-        <p className="text-white text-center">Welcome to Project Mars, your ultimate destination for delving into the captivating world of the Red Planet. Embark on an extraordinary journey as we bring you the latest and most intriguing information about Mars, all in one place. Whether you are an avid space enthusiast or just someone curious about the cosmos, Project Mars is here to satisfy your thirst for knowledge and wonder.
+        <p className="text-white text-center lg:pb-6 xl:w-4/5 self-center tracking-wide leading-6 md:text-lg 4k:text-xl">Welcome to Project Mars, your ultimate destination for delving into the captivating world of the Red Planet. Embark on an extraordinary journey as we bring you the latest and most intriguing information about Mars, all in one place. Whether you are an avid space enthusiast or just someone curious about the cosmos, Project Mars is here to satisfy your thirst for knowledge and wonder.
         </p>
-        <button className="py-4 my-6 button__style">About Mars</button>
+        <Link to="/about" className="flex justify-center">
+          <button className="py-4 my-6 button__style w-full lg:w-1/2">About Mars</button>
+        </Link>
       </div>
-      <img src="src/assets/M2.png" className="w-full h-full" />
+      <div className="xl:w-1/2">
+        <img src="src/assets/M2.png" className="" />
+      </div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,24 @@ export default {
   ],
   theme: {
     extend: {},
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+      '4k': '2560px',
+      // => @media (min-width: 2560px) { ... }
+    },
   },
   plugins: [],
 }
