8e240ab249193968856c11f3180ca53290692766
Cristian Sîrbu <cristiansirbu4@gmail.com>
2023-08-22 13:14:59 +0300

    Added comments in Model component for better code readability

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -6,18 +6,25 @@ import { useEffect, useRef } from 'react';
 
 
 
+/**
+ * Renders a 3D model using the Three.js library.
+ * @param {Object} props - The component props.
+ * @param {string} props.modelPath - The path to the 3D model file.
+ * @returns {JSX.Element} - The rendered component.
+ */
 const Model = ({ modelPath }) => {
   const containerRef = useRef(null);
 
-
   useEffect(() => {
+    // Set up the scene, camera, and renderer
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
     const renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
-    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setSize(window.innerWidth, window.innerHeight / 2);
     containerRef.current.appendChild(renderer.domElement);
-    camera.position.set(0, 0, 10);
+    camera.position.set(0, 0, 8);
 
+    // Added lights to the scene
     const leftLights = new THREE.DirectionalLight(0xffffff, 4);
     const rightLights = new THREE.DirectionalLight(0xffffff, 4);
     const backLights = new THREE.DirectionalLight(0xffffff, 4);
@@ -26,42 +33,38 @@ const Model = ({ modelPath }) => {
     rightLights.position.set(10, 10, 10);
     backLights.position.set(0, 0, -10);
     bottomLights.position.set(0, -10, 10);
-    scene.add(leftLights);
-    scene.add(rightLights);
-    scene.add(backLights);
-    scene.add(bottomLights);
+    scene.add(leftLights, rightLights, backLights, bottomLights);
 
-
+    // Loaded the 3D model
     const loader = new GLTFLoader();
     loader.load(modelPath, (gltf) => {
       scene.add(gltf.scene);
-
     }, undefined, (error) => {
       console.log(error);
     });
-    
+
+    // Added orbit controls
     const controls = new OrbitControls(camera, renderer.domElement);
+    controls.enableDamping = true;
+    controls.rotateSpeed = 0.4;
+
+    // Added animation loop
     const animate = () => {
       requestAnimationFrame(animate);
       renderer.render(scene, camera);
       scene.rotation.y += 0.001;
-
-      
-      controls.enableDamping = true;
-      controls.rotateSpeed = 0.4;
-
     }
-
     animate();
 
+    // Handled window resize
     const handleResize = () => {
       camera.aspect = window.innerWidth / window.innerHeight;
       camera.updateProjectionMatrix();
       renderer.setSize(window.innerWidth, window.innerHeight);
     }
-
     window.addEventListener('resize', handleResize);
 
+    // Cleaned up resources
     return () => {
       renderer.domElement.remove();
       renderer.dispose();
@@ -70,10 +73,12 @@ const Model = ({ modelPath }) => {
     };
   }, [modelPath]);
 
-
-  return <div ref={containerRef}></div>;
+  return (
+    <div className=''>
+      <div ref={containerRef} className=''></div>
+    </div>
+  );
 };
-
 
 Model.propTypes = {
   modelPath: PropTypes.string.isRequired
