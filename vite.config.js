8d8ae1f2fb71cf9ff7767e5e6c985d52889e6b6b
Cristian Sîrbu <cristiansirbu4@gmail.com>
2023-09-23 15:07:07 +0300

    Changed structure

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,11 @@ import { defineConfig } from 'vite'
 import compression from 'vite-plugin-compression2';
 import react from '@vitejs/plugin-react'
 
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), compression({algorithm: 'gzip'})],
+  plugins: [react(), compression({ algorithm: 'gzip' })],
   server: {
     proxy: {
-      '/daily-weather': 'http://localhost:3000', 
+      '/daily-weather': 'http://localhost:3000',
     },
-  },
+  }
 })
