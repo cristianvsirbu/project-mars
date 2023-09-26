db3f4db11e0f57a6f80f8d79ed8fc57ce1fcdd06
Cristian Sîrbu <cristiansirbu4@gmail.com>
2023-09-26 15:50:45 +0300

    added useMemo in imports

--- client/src/components/routes/Weather.jsx
+++ client/src/components/routes/Weather.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useMemo } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import barometer from '/assets/weather/barometer.svg';
