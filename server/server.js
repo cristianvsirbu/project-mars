c417073844221484e2a5aab8f04a9c22407323b2
Cristian Sîrbu <cristiansirbu4@gmail.com>
2023-09-26 10:55:05 +0300

    Added a catch-all route

--- server/server.js
+++ server/server.js
@@ -3,15 +3,15 @@ const serverless = require('serverless-http');
 const cors = require('cors');
 const scraper = require('./scraper');
 const { Router } = require('express');
-
-
 const app = express();
 const port = process.env.PORT || 3000;
+const router = Router();
 
 // Enable CORS for all routes
 app.use(cors());
 
-const router = Router();
+// Serve existing "public" directory for static assets.
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Define the route to fetch daily weather data
 router.get('/daily-weather', async (req, res) => {
@@ -25,10 +25,16 @@ router.get('/daily-weather', async (req, res) => {
     }
 });
 
+app.get('*', (req, res) => {
+    // Serve index.html for any other requests.
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 app.use(express.json(), router);
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
 
 // Export as a serverless function
 module.exports.handler = serverless(app);
