3b3e546e277d244852d6dd0c1d9a8cc6d271c3e5
Cristian Sîrbu <cristiansirbu4@gmail.com>
2023-09-26 15:16:46 +0300

    changed directory to __dirname

--- server/.puppeteerrc.cjs
+++ server/.puppeteerrc.cjs
@@ -5,5 +5,5 @@ const { join } = require('path');
  */
 module.exports = {
     // Changes the cache location for Puppeteer.
-    cacheDirectory: join('/opt/render/project/server', '.cache', 'puppeteer'),
+    cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
 };
