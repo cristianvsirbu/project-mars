b2b4c8103a8da498b06215bf6e25e3c96c4d152b
Cristian Sîrbu <cristiansirbu4@gmail.com>
2023-09-26 10:55:05 +0300

    Added a catch-all route

--- client/src/components/routes/About.jsx
+++ client/src/components/routes/About.jsx
@@ -65,29 +65,3 @@ const About = () => {
 };
 
 export default About;
-
-
-
-// about
-// about / mars
-// about / moons
-//         moons / phobos
-//         moons / deimos
-
-// about / rovers
-//       / rovers / active
-//                / active/ curiosity
-//       / rovers / inactive
-//                / inactive/ opportunity
-//       / rovers / planned
-//                / planned/ spirit
-//       / rovers / failed
-//                / failed/ mars2
-
-// about / satellites
-//       / satellites/ functional
-//                   / functional/ maven
-//       / satellites/ non-functional
-//                   / non-functional/ maven2
-
-// about / missions
