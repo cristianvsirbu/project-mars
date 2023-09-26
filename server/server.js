869d98df66163b1bd48e6385c19d8c777a5afd4d
Cristian Sîrbu <cristiansirbu4@gmail.com>
2023-09-26 16:15:37 +0300

    changed path

--- server/server.js
+++ server/server.js
@@ -42,7 +42,7 @@ router.get('/daily-weather', async (req, res) => {
 
 app.get('*', (_req, res) => {
     // Serve index.html for any other requests.
-    res.sendFile(path.join(__dirname, '/client', 'index.html'));
+    res.sendFile(path.join('/client/index.html'));
 });
 
 app.use(express.json(), router);
