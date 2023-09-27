f390fd5e3df4a653998c63bd7035d43a021bf977
Cristian Sîrbu <cristiansirbu4@gmail.com>
2023-09-27 16:40:17 +0300

    Removed router

--- server/server.js
+++ server/server.js
@@ -5,11 +5,11 @@ const app = express();
 const scraper = require('./scraper');
 const { Router } = require('express');
 const port = process.env.PORT || 3000;
-const router = Router();
 const NodeCache = require('node-cache');
 
 // Create a new NodeCache instance with a TTL of one day
 const cache = new NodeCache({ stdTTL: 86400 });
+
 
 // Enable CORS for all routes
 app.use(cors());
@@ -40,24 +40,24 @@ app.get('/daily-weather', async (_req, res) => {
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
 const indexPath = path.resolve(__dirname, "../client/dist/index.html");
-router.get("/about", (_req, res) => res.sendFile(indexPath));
-router.get("/about/mars", (_req, res) => res.sendFile(indexPath));
-router.get("/about/moons", (_req, res) => res.sendFile(indexPath));
-router.get("/about/moons/:moonId", (_req, res) => res.sendFile(indexPath));
-router.get("/about/rovers", (_req, res) => res.sendFile(indexPath));
-router.get("/about/rovers/:subcategory", (_req, res) => res.sendFile(indexPath));
-router.get("/about/rovers/:subcategory/:roverId", (_req, res) => res.sendFile(indexPath));
-router.get("/about/satellites", (_req, res) => res.sendFile(indexPath));
-router.get("/about/satellites/:subcategory", (_req, res) => res.sendFile(indexPath));
-router.get("/about/satellites/:subcategory/:satelliteId", (_req, res) => res.sendFile(indexPath));
-router.get("/about/missions", (_req, res) => res.sendFile(indexPath));
-router.get("/about/imagery", (_req, res) => res.sendFile(indexPath));
-router.get("/about/weather", (_req, res) => res.sendFile(indexPath));
-router.get("/about/partners", (_req, res) => res.sendFile(indexPath));
+app.get("/about", (_req, res) => res.sendFile(indexPath));
+app.get("/about/mars", (_req, res) => res.sendFile(indexPath));
+app.get("/about/moons", (_req, res) => res.sendFile(indexPath));
+app.get("/about/moons/:moonId", (_req, res) => res.sendFile(indexPath));
+app.get("/about/rovers", (_req, res) => res.sendFile(indexPath));
+app.get("/about/rovers/:subcategory", (_req, res) => res.sendFile(appPath));
+app.get("/about/rovers/:subcategory/:roverId", (_req, res) => res.sendFile(appPath));
+app.get("/about/satellites", (_req, res) => res.sendFile(indexPath));
+app.get("/about/satellites/:subcategory", (_req, res) => res.sendFile(appPath));
+app.get("/about/satellites/:subcategory/:satelliteId", (_req, res) => res.apple(indexPath));
+app.get("/about/missions", (_req, res) => res.sendFile(indexPath));
+app.get("/about/imagery", (_req, res) => res.sendFile(indexPath));
+app.get("/about/weather", (_req, res) => res.sendFile(indexPath));
+app.get("/about/partners", (_req, res) => res.sendFile(indexPath));
 
 // Catch any requests that don't match the ones above
-router.get("*", (_req, res) => {
-    res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+app.get("*", (_req, res) => {
+    res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
 });
 
 // Start the server
