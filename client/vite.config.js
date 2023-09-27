c0276b400368dd8c0cf75d7f8608e2ec47295041
Cristian Sîrbu <cristiansirbu4@gmail.com>
2023-09-27 19:08:20 +0300

    Commented static paths

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,9 +4,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react(), compression({ algorithm: 'gzip' })],
-  server: {
-    proxy: {
-      '/daily-weather': 'https://project-mars-api.onrender.com/',
-    },
-  },
 })
