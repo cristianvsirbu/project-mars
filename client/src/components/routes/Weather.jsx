d00ecac4a1b239c533020cd1c98aaff66100885f
Cristian Sîrbu <cristiansirbu4@gmail.com>
2023-09-29 11:39:01 +0300

    changed path back to relative

--- client/src/components/routes/Weather.jsx
+++ client/src/components/routes/Weather.jsx
@@ -118,7 +118,7 @@ const Weather = () => {
   const [error, setError] = useState(false);
 
   useEffect(() => {
-    fetch("https://project-mars.onrender.com/daily-weather")
+    fetch("/daily-weather")
       .then(res => {
         if (res.ok) {
           return res.json();
