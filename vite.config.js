2deca76d46b9dc10afe1bd38e4f31114e3f0e3f4
Cristian Sîrbu <cristiansirbu4@gmail.com>
2023-10-01 17:56:46 +0300

    Removed vercel-related files, tried with netlify

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react(), compression({ algorithm: 'gzip' })],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:3000/api', 
-    },
-  }
 })
