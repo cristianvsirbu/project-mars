d7fbce10a45f0b60ee832be7ca6ea06878a1ddfc
Cristian Sîrbu <cristiansirbu4@gmail.com>
2025-05-06 16:34:33 +0300

    fix: downgrade react and related packages to maintain compatibility

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react(), compression({ algorithm: 'gzip' })],
-  assetsInclude: ['**/*.html'],
   server: {
     port: 3000
   },
