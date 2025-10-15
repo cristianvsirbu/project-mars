18ddceeda2dd17fe1ea2bca93c4e84c09fdef282
Cristian Sîrbu <cristiansirbu4@gmail.com>
2025-10-15 12:18:03 +0300

    fix: update Navbar styles for improved text sizing and layout consistency

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -30,7 +30,7 @@ const Navbar = () => {
     if (route.children) {
       return route.children.map((childRoute) => (
         <li
-          className={`text-white blink cursor-pointer font-bold py-2 md:py-0 text-[32px] md:text-[20px] lg:text-[24px] xl:text-[1.5rem]`}
+          className={`text-white blink cursor-pointer font-bold py-2 md:py-0 text-[2rem] md:text-[1.2rem] xl:text-[1.5rem]`}
           key={childRoute.path}
           onClick={handleItemClick}
         >
@@ -46,17 +46,17 @@ const Navbar = () => {
   return (
     <div className="navbar select-none">
       {/* Navbar */}
-      <nav className="mx-6 h-20 flex justify-between md:mx-16 md:h-14 lg:h-16">
+      <nav className="mx-6 h-20 flex lg:justify-evenly md:mx-16 lg:mx-32 md:h-14 lg:h-16">
         {/* Logo */}
-        <div className="mt-3 h-auto max-w-[6rem] md:max-w-[4.5rem] lg:max-w-[6rem] lg:mt-2 xl:ml-[4rem] xl:max-w-[6rem]">
+        <div className="mt-3 h-full md:w-1/4 flex items-center justify-center">
           <Link to="/" aria-label="Go to Home">
-            <img src="/assets/logos/logo.webp" alt="Logo" />
+            <img src="/assets/logos/logo.webp" className="w-24 md:w-20 xl:w-24" alt="Logo" />
           </Link>
         </div>
 
         {/* Menu */}
         <ul
-          className={`hidden justify-between items-center md:flex md:w-[75%] lg:w-[70%] 2xl:justify-around`}
+          className={`hidden md:justify-between items-center md:flex md:w-[80%] lg:w-[60%] xl:justify-evenly`}
         >
           {navLinks}
         </ul>
