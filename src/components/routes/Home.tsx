26fa7d7b255e15b68cd6b1ac2bd062da42def419
Cristian Sîrbu <cristiansirbu4@gmail.com>
2025-10-15 12:15:25 +0300

    fix: add responsive max-width to Mars image for better layout

--- src/components/routes/Home.tsx
+++ src/components/routes/Home.tsx
@@ -39,7 +39,7 @@ const Home = () => {
         </div>
       </div>
       <div className="z-1 flex justify-center items-center w-full">
-        <img src="/assets/mars/M2.webp" alt="Mars" />
+        <img src="/assets/mars/M2.webp" className='max-w-[75%] xl:max-w-full' alt="Mars" />
       </div>
     </div>
   );
