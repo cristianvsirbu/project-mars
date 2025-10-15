44824b9bd518fb45edd7e50ec7a11fd29859e284
Cristian Sîrbu <cristiansirbu4@gmail.com>
2025-10-15 12:32:57 +0300

    fix: adjust layout of Home component for improved responsiveness

--- src/components/routes/Home.tsx
+++ src/components/routes/Home.tsx
@@ -7,7 +7,7 @@ const Home = () => {
       <div className="sun hidden md:block md:left-[40rem] md:top-[-24rem] lg:left-[58rem] lg:top-[-26rem] xl:left-[50rem] xl:top-[-22rem]" />
 
       <div className="p-4 flex w-full flex-col lg:justify-center">
-        <div className="py-8 px-4 z-10 w-[95%] mx-auto flex flex-col md:my-10 md:w-[90%] lg:py-16 xl:px-0 xl:w-[80%] blur__card">
+        <div className="py-8 px-4 z-10 w-[70%] mx-auto flex flex-col md:my-10 lg:py-16 xl:px-0 blur__card">
           {/* Title */}
           <div className="flex flex-col items-center pb-4">
             <h2 className="text-white font-bold text-3xl 4k:text-[3rem]">PROJECT</h2>
@@ -29,13 +29,11 @@ const Home = () => {
             just someone curious about the cosmos, Project Mars is here to satisfy your thirst for
             knowledge and wonder.
           </p>
-          <div className="flex justify-center">
-            <button className="mt-4">
+            <button className="">
               <Link to="/about/mars" className="button__style  inline-block relative">
                 <span className="">ABOUT MARS</span>
               </Link>
             </button>
-          </div>
         </div>
       </div>
       <div className="z-1 flex justify-center items-center w-full">
