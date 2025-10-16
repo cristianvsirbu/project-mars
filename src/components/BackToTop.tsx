36a54f9064569416891d230fa7713fb365c45360
Cristian Sîrbu <cristiansirbu4@gmail.com>
2025-10-16 13:42:46 +0300

    fix: remove redundant arrow images from BackToTop component

--- src/components/BackToTop.tsx
+++ src/components/BackToTop.tsx
@@ -42,17 +42,7 @@ const BackToTop = () => {
                         `}
       onClick={handleClick}
     >
-      <img
-        src="/assets/up.svg"
-        alt="Back to Top"
-        className="hidden lg:block right-[1.5rem] top-[1rem] arrow"
-      />
       <span>Back to Top</span>
-      <img
-        src="/assets/up.svg"
-        alt="Back to Top"
-        className="hidden lg:block left-[1.5rem] top-[1rem] arrow"
-      />
     </button>
   );
 };
