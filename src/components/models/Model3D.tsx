e7d95bb51b3ebaca9abd0ebe1fa0df3eda5b9b49
Cristian Sîrbu <cristiansirbu4@gmail.com>
2025-10-20 13:53:16 +0300

    Implement code changes to enhance functionality and improve performance

--- src/components/models/Model3D.tsx
+++ src/components/models/Model3D.tsx
@@ -1,5 +1,5 @@
 import * as THREE from 'three';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { useEffect, useRef, useState } from 'react';
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
