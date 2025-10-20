2b1f3a1275f445f98db1006b1bcfddc02966deaf
Cristian Sîrbu <cristiansirbu4@gmail.com>
2025-10-20 13:53:48 +0300

    fix: update import path for DRACOLoader to include file extension

--- src/components/models/Model3D.tsx
+++ src/components/models/Model3D.tsx
@@ -2,7 +2,7 @@ import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { useEffect, useRef, useState } from 'react';
-import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js';
 import { GLTF } from 'three/examples/jsm/loaders/GLTFLoader';
 
 // Global renderer manager
