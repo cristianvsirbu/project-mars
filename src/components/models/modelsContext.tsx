deeee762bf2b8f045991b5e2d0a088e29a8fadee
Cristian Sîrbu <cristiansirbu4@gmail.com>
2025-12-12 10:30:34 +0200

    fix: remove duplicate import of ReactNode in modelsContext.tsx

--- src/components/models/modelsContext.tsx
+++ src/components/models/modelsContext.tsx
@@ -1,8 +1,7 @@
-import { createContext } from 'react';
+import { createContext, ReactNode } from 'react';
 import { modelsData } from './modelsData';
 
 export const ModelsDataContext = createContext(modelsData);
-import { ReactNode } from 'react';
 
 export const ModelsDataContextProvider = ({ children }: { children: ReactNode }) => {
   return <ModelsDataContext.Provider value={modelsData}>{children}</ModelsDataContext.Provider>;
