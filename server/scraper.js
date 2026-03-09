4aae1c4a057d840c35507113d7e9179cc7b038c3
Cristian Sîrbu <cristiansirbu4@gmail.com>
2026-03-09 16:53:10 +0200

    Refactor scraper function to update weather data extraction and improve code readability

--- server/scraper.js
+++ server/scraper.js
@@ -2,9 +2,9 @@ const puppeteer = require('puppeteer');
 const chromium = require('chrome-aws-lambda');
 
 async function scraper() {
-     let browser;
-     try {
-       browser = await puppeteer.launch({
+    let browser;
+    try {
+        browser = await puppeteer.launch({
             args: chromium.args,
             executablePath: await chromium.executablePath,
             headless: 'new',
@@ -12,37 +12,40 @@ async function scraper() {
         });
         let page = await browser.newPage();
         page.setDefaultNavigationTimeout(2 * 60 * 1000);
-        await page.goto("https://mars.nasa.gov/msl/weather/", { waitUntil: "domcontentloaded" });
-        await page.waitForSelector('#weather_observation tbody tr');
+        await page.goto("https://mars.nasa.gov/layout/embed/image/mslweather/", { waitUntil: "domcontentloaded" });
+        await page.waitForSelector('#Forecast');
 
         let weatherData = await page.evaluate(() => {
             const forecastRows = Array.from(document.querySelectorAll('#Forecast .item'));
-            const observationRows = Array.from(document.querySelectorAll('#weather_observation tbody tr'));
 
-            const data = forecastRows.map((forecastRow, index) => {
+            return forecastRows.map((forecastRow) => {
                 const dateSol = forecastRow.querySelector('.dateSol')?.textContent.trim().replace(/^Sol\s+/i, '');
-                const UTC = observationRows[observationRows.length - 1 - index]?.querySelector('.sol')?.textContent.trim();
-                const highCelsius = observationRows[observationRows.length - 1 - index]?.querySelector('.temperature.max .celsius')?.textContent.trim();
-                const lowCelsius = observationRows[observationRows.length - 1 - index]?.querySelector('.temperature.min .celsius')?.textContent.trim();
-                const pressure = observationRows[observationRows.length - 1 - index]?.querySelector('.pressure.max')?.textContent.trim();
-                const sunrise = observationRows[observationRows.length - 1 - index]?.querySelector('.sun.rise')?.textContent.trim();
-                const sunset = observationRows[observationRows.length - 1 - index]?.querySelector('.sun.set')?.textContent.trim();
+                const UTC = forecastRow?.querySelector('.dateUTC')?.textContent.trim();
+                const highCelsius = forecastRow
+                    .querySelector('.celsius .high')
+                    ?.textContent.trim()
+                    .replace('High: ', '');
+                const lowCelsius = forecastRow
+                    .querySelector('.celsius .low')
+                    ?.textContent.trim()
+                    .replace('Low: ', '');
+                // const pressure = forecastRow?.querySelector('.pressure.max')?.textContent.trim();
+                // const sunrise = forecastRow?.querySelector('.sun.rise')?.textContent.trim();
+                // const sunset = forecastRow?.querySelector('.sun.set')?.textContent.trim();
 
-                return { dateSol, pressure, sunrise, sunset, UTC, highCelsius, lowCelsius };
+                return { dateSol, UTC, highCelsius, lowCelsius };
             });
-
-            return data;
         });
         await browser.close();
         return weatherData;
     } catch (error) {
         console.error(error);
+        throw error;
     } finally {
         if (browser) {
             await browser.close();
         }
     }
 }
-scraper();
 
 module.exports = scraper;
