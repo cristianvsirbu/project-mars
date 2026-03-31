79ccc6eee3581d3950828090efdcf1946c10c3a1
Cristian Sîrbu <cristiansirbu4@gmail.com>
2026-03-31 13:12:20 +0300

    refactor: update ParallaxCard styles for improved layout and responsiveness

--- src/components/ui/ParallaxCard.tsx
+++ src/components/ui/ParallaxCard.tsx
@@ -3,10 +3,14 @@ import { ParallaxCardProps } from '../../lib/types';
 
 const ParallaxCard = ({ link, imgSrc, title }: ParallaxCardProps) => {
   return (
-    <div className="m-2 w-[12rem] h-[20rem] md:w-[12rem] md:h-[20rem] lg:w-[15rem] lg:h-[25rem] blur__card parallax">
+    <div className="m-2 w-[12rem] h-[20rem] md:w-[12rem] md:h-[20rem] lg:w-[16rem] lg:h-[25rem] blur__card parallax">
       <Link to={link} className="flex flex-col h-full justify-between py-10">
-        <img src={imgSrc} className="w-[9rem] lg:w-[12rem] self-center" alt={title} />
-        <span className="blink text-3xl lg:text-4xl"> {title} </span>
+        <img
+          src={imgSrc}
+          className="w-[9rem] lg:w-[12rem] h-[9rem] lg:h-[12rem] object-contain self-center"
+          alt={title}
+        />
+        <span className="blink text-2xl lg:text-3xl px-4"> {title} </span>
       </Link>
     </div>
   );
