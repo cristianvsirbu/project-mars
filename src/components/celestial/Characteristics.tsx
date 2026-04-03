f24bfca1ded07cea2533f27d7b4e90526e806bf3
Cristian Sîrbu <cristiansirbu4@gmail.com>
2026-04-03 12:31:08 +0300

    fix: use label as key in Characteristics component for better uniqueness

--- src/components/celestial/Characteristics.tsx
+++ src/components/celestial/Characteristics.tsx
@@ -4,7 +4,7 @@ const Characteristics = ({ data }: { data: Characteristic[] }) => {
   return (
     <div className="text-base lg:text-lg 4k:text-xl lg:max-w-[40%]">
       {data.map(({ label, value }) => (
-        <div key={value} className="flex flex-wrap">
+        <div key={label} className="flex flex-wrap">
           <strong className="text-orange-500 mr-2">{label}:</strong>
           <p className="text-slate-400 font-medium">{value}</p>
         </div>
